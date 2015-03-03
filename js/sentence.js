a9f03863882a8a208e8b51b9e2b79c541d10154f
mike_ng <junta.m.ng@gmail.com>
2015-03-03 17:59:21 -0500

    add testing

---
+++ js/sentence.js
@@ -0,0 +1,106 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Mike Ng
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+;(function ( $, window, document, undefined ) 
+{
+	"use strict";
+       
+    var pluginName = "sentence",
+	defaults = {
+		
+	};
+
+	function Plugin ( element, options ) 
+	{
+        var that = this;
+		this.element = element;
+		this.$element = $(element);
+		this.$sentence = this.$element.find(".s");
+        this.$blank = this.$element.find(".b");				
+		this.settings = $.extend( {}, defaults, options );
+		this._defaults = defaults;
+		this._name = pluginName;		
+
+        this.init();
+	}
+
+	$.extend(Plugin.prototype, 
+	{
+		init: function() 
+		{		
+            this.sSentenceText = this.$sentence.text();
+            this.iSentenceSize = parseInt(this.$sentence.css("font-size"));
+            this.sBlankText = this.$blank.text();
+            this.iBlankSize = parseInt(this.$blank.css("font-size"));
+            this.sBlankFontFamily = parseInt(this.$blank.css("font-family"));
+            this.sBlankFontFamily = "monospace";
+            this.$element.empty();
+            this.$element.append("<div class='s_wrap'>" +
+                    "<span class='s_s'>" + this.sSentenceText + "</span> " +
+                    "<span class='b_wrap'>" +
+                        "<span class='b_underline'>__________.</span>" +
+                        "<span class='b_b'>" + this.sBlankText + "</span>" + 
+                    "</span>" +                
+                    "<TEXTAREA class='b_t'>" + this.sBlankText + "</TEXTAREA>" +
+                "</div>"
+            );    
+
+            this.$sentence = this.$element.find(".s_s");
+            this.$underline = this.$element.find(".b_underline"); 
+            this.$blank = this.$element.find(".b_b"); 
+            this.$textarea = this.$element.find(".b_t"); 
+            this.$sentence.css({"font-size": this.iSentenceSize + "px"});
+            this.$underline.css({"font-size": this.iSentenceSize + "px"});
+            this.$blank.css({"font-size": this.iBlankSize + "px"});
+            
+            var iTop = -(this.$blank.innerHeight()) / 2;
+            this.$blank.css({"font-size": this.iBlankSize + "px", "font-family": this.sBlankFontFamily, "margin-top": iTop+"px"});            
+            this.$textarea.css({"font-size": this.iBlankSize + "px", "font-family": this.sBlankFontFamily, "margin-top": iTop+"px", "text-indent": this.$sentence.outerWidth()+3 });
+            
+
+		},
+		
+        checkObjProp: function() {
+            var sKey, iCnt=0;
+            for (sKey in this.arr)
+            {
+                if (this.arr.hasOwnProperty(sKey))
+                    iCnt++;
+            }
+            //console.log("prop count = " + iCnt);
+        }
+	});
+
+	$.fn[ pluginName ] = function ( options ) 
+	{
+		return this.each(function() 
+		{
+			if ( !$.data( this, "plugin_" + pluginName ) ) 
+			{
+					$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+			}
+		});
+	};
+
+})( jQuery, window, document );
