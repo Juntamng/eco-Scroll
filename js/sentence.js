4e49786f717a034a539bd383e9943b9c1a979749
unknown <junta.m.ng@example>
2015-03-04 01:21:37 -0500

    working on testing folder

--- js/sentence.js
+++ js/sentence.js
@@ -28,7 +28,14 @@ SOFTWARE.
        
     var pluginName = "sentence",
 	defaults = {
-		
+		onEdit: function(oParam) 
+        {
+            return true;
+        },
+        onHide: function(oParam) 
+        {
+
+        }
 	};
 
 	function Plugin ( element, options ) 
@@ -40,7 +47,17 @@ SOFTWARE.
         this.$blank = this.$element.find(".b");				
 		this.settings = $.extend( {}, defaults, options );
 		this._defaults = defaults;
-		this._name = pluginName;		
+		this._name = pluginName;
+
+        this.sSentenceText = undefined;
+        this.iSentenceSize = undefined;
+        this.sBlankText = undefined;
+        this.iBlankSize = undefined;
+        this.sBlankFontFamily = undefined;
+        this.iBlankHeight = 0;
+        this.$wrap = undefined;
+        this.$underline = undefined; 
+        this.$textarea = undefined;
 
         this.init();
 	}
@@ -49,46 +66,103 @@ SOFTWARE.
 	{
 		init: function() 
 		{		
+            var that = this;
             this.sSentenceText = this.$sentence.text();
             this.iSentenceSize = parseInt(this.$sentence.css("font-size"));
             this.sBlankText = this.$blank.text();
             this.iBlankSize = parseInt(this.$blank.css("font-size"));
             this.sBlankFontFamily = parseInt(this.$blank.css("font-family"));
             this.sBlankFontFamily = "monospace";
-            this.$element.empty();
-            this.$element.append("<div class='s_wrap'>" +
-                    "<span class='s_s'>" + this.sSentenceText + "</span> " +
+            
+            this.$element
+            .empty()
+            .append("<div class='s_wrap'>" +                    
+                    "<span class='s_s'>" + this.sSentenceText + "</span> " +                    
                     "<span class='b_wrap'>" +
-                        "<span class='b_underline'>__________.</span>" +
-                        "<span class='b_b'>" + this.sBlankText + "</span>" + 
-                    "</span>" +                
-                    "<TEXTAREA class='b_t'>" + this.sBlankText + "</TEXTAREA>" +
-                "</div>"
+                        "<span class='b_underline'>__________.</span>" +                        
+                    "</span>" +             
+                    "<span class='b_b'>" + this.sBlankText + "</span>" +                        
+                "</div><div style='clear:both'></div>"
             );    
 
+            this.$wrap = this.$element.find(".s_wrap");
             this.$sentence = this.$element.find(".s_s");
             this.$underline = this.$element.find(".b_underline"); 
             this.$blank = this.$element.find(".b_b"); 
-            this.$textarea = this.$element.find(".b_t"); 
+
             this.$sentence.css({"font-size": this.iSentenceSize + "px"});
             this.$underline.css({"font-size": this.iSentenceSize + "px"});
-            this.$blank.css({"font-size": this.iBlankSize + "px"});
             
-            var iTop = -(this.$blank.innerHeight()) / 2;
-            this.$blank.css({"font-size": this.iBlankSize + "px", "font-family": this.sBlankFontFamily, "margin-top": iTop+"px"});            
-            this.$textarea.css({"font-size": this.iBlankSize + "px", "font-family": this.sBlankFontFamily, "margin-top": iTop+"px", "text-indent": this.$sentence.outerWidth()+3 });
-            
-
-		},
-		
-        checkObjProp: function() {
-            var sKey, iCnt=0;
-            for (sKey in this.arr)
+            var bIsEmpty = false;
+            // if blank is empty, add dummy char "A" for getting the height
+            if ( $.trim(this.sBlankText).length == 0 )
             {
-                if (this.arr.hasOwnProperty(sKey))
-                    iCnt++;
+                bIsEmpty = true;
+                this.$blank.text("A");
             }
-            //console.log("prop count = " + iCnt);
+
+            this.$blank.css({
+                "text-indent": this.$sentence.outerWidth()+3,
+                "font-size": this.iBlankSize + "px", 
+                "font-family": this.sBlankFontFamily, 
+            });
+            
+            this.iBlankHeight = this.$blank.innerHeight();
+            var iTop = -(this.iBlankHeight) / 2;
+            this.$blank.css({                
+                "margin-top": iTop+"px"
+            });                        
+
+            // remove dummy char "A"
+            if (bIsEmpty == true) this.$blank.text("");
+            
+            this.$wrap
+            .on("click", function()
+            {
+                that.edit();
+            })            
+		},		
+        edit: function() 
+        {
+            var that = this;
+
+            if ( !that.settings.onEdit({$element: that.$element}) )
+                return false;
+
+            if (this.$textarea)
+                ;
+            else
+            {
+                this.$textarea = $("#b_textarea");
+                if (this.$textarea.length == 0)
+                {
+                    this.$textarea = $("<textarea id='b_textarea' class='b_t'></textarea>")
+                    .click(function(e)
+                    {
+                        e.preventDefault();
+                        e.stopPropagation();
+                    });
+                }
+            }
+
+            var iTop = -(this.iBlankHeight) / 2;
+            
+            this.$blank.hide();
+            this.$textarea.val(this.$blank.text())
+            .css({"font-size": this.iBlankSize + "px", "font-family": this.sBlankFontFamily, "margin-top": iTop+"px", "text-indent": this.$sentence.outerWidth()+3 })
+            .appendTo(this.$wrap)
+            .one("blur", function() {
+                that.settings.onHide({$element: that.$element});
+
+                that.$textarea.hide().appendTo(document.body);
+                that.$blank.text(that.$textarea.val()).show();
+            })
+            .show()
+            .focus();            
+        },
+        destroy: function() 
+        {
+            this.$wrap.off("click");
         }
 	});
 
